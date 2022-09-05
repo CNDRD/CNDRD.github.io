2a249f7f7a17c18268ec9a6e09b8d68a7c466cd5
CNDRD <michal.mikulec.mm@gmail.com>
2022-09-06 00:45:12 +0200

    siegeapi docs v1 finished

--- siegeapi/index.js
+++ siegeapi/index.js
@@ -12,15 +12,37 @@ function changeVersion(version=0) {
             $(cls).hide();
         }
     });
-}
+};
+
+function scrollTo(where=undefined) {
+    if (where === undefined) {
+        where = $(location).attr('hash');
+    }
+
+    where = where.replaceAll("#", "").split("-");
+
+    let where_cls = where[0];
+    let where_ver = where[1] || $("#version-select").val();
+
+    $(`[data-class-name]`).each( (i, cls) => {
+        let class_versions = $(cls).attr('data-versions').split(",");
+
+        if ($(cls).attr('data-class-name').toLowerCase() == where_cls.toLowerCase() && class_versions.includes(where_ver)) {
+            
+            $('html, body').animate({ scrollTop: $(cls).offset().top }, 750);
+
+            $(cls).addClass("highlight");
+            setTimeout(() => { $(cls).removeClass("highlight"); }, 7500);
+        }
+    });
+};
 
 
 $("#version-select").change( (e) => {
     changeVersion($("#version-select").val());
 });
 
-
 $(document).ready( () => {
     changeVersion();
+    scrollTo();
 });
- 
